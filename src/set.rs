b91c095274c362520798de173841847ca3d71418
Calum Leslie <calumleslie@gmail.com>
2015-11-01 17:59:55 +0000

    Implement the unbalanced set type (binary search tree).
    Having to return self.clone() when the item is unchanged is disappointing.
    If the methods were implemented on Arc<Tree<T>> it would work better but
    this is a little inconvienient.

---
+++ src/set.rs
@@ -0,0 +1,78 @@
+use std::sync::Arc;
+
+trait Set<T: Ord> {
+	fn empty() -> Self;
+	fn insert(&self, value: T) -> Self;
+	fn member(&self, value: T) -> bool;
+}
+
+#[derive(Debug, Clone)]
+enum Tree<T: Ord + Clone> {
+	Empty,
+	Node { left: Arc<Tree<T>>, value: T, right: Arc<Tree<T>> }
+}
+
+impl <T: Ord + Clone> Set<T> for Tree<T> {
+	fn empty() -> Self {
+		return Tree::Empty;
+	}
+	fn insert(&self, new_value: T) -> Self {
+		return match *self {
+			Tree::Empty => {
+				let empty = Arc::new( Tree::empty() );
+				Tree::Node { 
+					left: empty.clone(),
+					right: empty.clone(),
+					value: new_value
+				}
+			},
+			Tree::Node { ref left, ref value, ref right } => {
+				if new_value < *value {
+					Tree::Node {
+						left: Arc::new( left.insert( new_value ) ),
+						value: value.clone(),
+						right: right.clone()
+					}
+				} else if new_value > *value {
+					Tree::Node {
+						left: left.clone(),
+						value: value.clone(),
+						right: Arc::new( right.insert( new_value ) )
+					}
+				} else {
+					// TODO: Would be good if this could return the original?
+					return self.clone();
+				}
+			}
+		}
+	}
+	fn member(&self, search_value: T) -> bool {
+		return match *self {
+			Tree::Empty => false,
+			Tree::Node { ref left, ref value, ref right } => if search_value < *value {
+				left.member(search_value)
+			} else if search_value > *value {
+				right.member(search_value)
+			} else {
+				true
+			}
+		}
+	}
+}
+
+#[test]
+fn empty_contains_nothing() {
+	let empty_tree: Tree<()> = Tree::empty();
+
+	// There is only one value of this type so this is exhaustive.
+	assert!( !empty_tree.member( () ) );
+}
+
+#[test]
+fn inserted_values_are_contained() {
+	let tree = Tree::empty().insert( 3 ).insert( 5 );
+
+	assert!( tree.member(3) );
+	assert!( tree.member(5) );
+	assert!( !tree.member(42) );
+}
