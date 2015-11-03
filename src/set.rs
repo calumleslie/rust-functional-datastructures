ee538756bc4c6b691867e3e584aa6d3eac98ebbc
Calum Leslie <calumleslie@gmail.com>
2015-11-03 20:12:45 +0000

    Run rustfmt on everything.
    Suggestions sensible, especially considering I don't know what on earth
    I'm doing. I could get used to this.

--- src/set.rs
+++ src/set.rs
@@ -2,101 +2,109 @@ use std::sync::Arc;
 use std::fmt::Debug;
 
 trait Set<T: Ord> {
-	fn empty() -> Self;
-	fn insert(&self, value: T) -> Self;
-	fn member(&self, value: T) -> bool;
+    fn empty() -> Self;
+    fn insert(&self, value: T) -> Self;
+    fn member(&self, value: T) -> bool;
 }
 
 #[derive(Debug, Clone)]
 enum Tree<T: Ord + Clone> {
-	Empty,
-	Node { left: Arc<Tree<T>>, value: T, right: Arc<Tree<T>> }
+    Empty,
+    Node {
+        left: Arc<Tree<T>>,
+        value: T,
+        right: Arc<Tree<T>>,
+    },
 }
 
 impl <T: Ord + Clone + Debug> Set<T> for Tree<T> {
-	fn empty() -> Self {
-		return Tree::Empty;
-	}
-	fn insert(&self, new_value: T) -> Self {
-		match *self {
-			Tree::Empty => Tree::singleton(new_value),
-			Tree::Node { ref value, .. } => self.
-				try_insert_with_candidate(new_value, value.clone()).
-				unwrap_or_else(||self.clone())
-		}
-	}
-	fn member(&self, search_value: T) -> bool {
-		return match *self {
-			Tree::Empty => false,
-			Tree::Node { ref value, .. } => self.member_with_candidate(search_value, value.clone())
-		}
-	}
+    fn empty() -> Self {
+        return Tree::Empty;
+    }
+    fn insert(&self, new_value: T) -> Self {
+        match *self {
+            Tree::Empty => Tree::singleton(new_value),
+            Tree::Node { ref value, .. } =>
+                self.try_insert_with_candidate(new_value, value.clone())
+                    .unwrap_or_else(|| self.clone()),
+        }
+    }
+    fn member(&self, search_value: T) -> bool {
+        return match *self {
+            Tree::Empty => false,
+            Tree::Node { ref value, .. } => self.member_with_candidate(search_value, value.clone()),
+        };
+    }
 }
 
 impl<T: Ord + Clone + Debug> Tree<T> {
-	fn member_with_candidate(&self, search_value:T, best_candidate: T) -> bool {
-		return match *self {
-			Tree::Empty => search_value == best_candidate,
-			Tree::Node { ref left, ref value, ref right } => if search_value < *value {
-				left.member_with_candidate(search_value, best_candidate)
-			} else {
-				right.member_with_candidate(search_value, value.clone())
-			}
-		}
-	}
-	fn try_insert_with_candidate(&self, new_value: T, candidate: T) -> Option<Self> {
-		match *self {
-			Tree::Empty => {
-				if new_value == candidate {
-					None
-				} else {
-					Some( Tree::singleton(new_value) )
-				}
-			},
-			Tree::Node { ref left, ref value, ref right } => {
-				if new_value < *value {
-					left.try_insert_with_candidate( new_value, candidate ).map(|new_left| Tree::Node {
-						left: Arc::new( new_left ),
-						value: value.clone(),
-						right: right.clone()
-					})					
-				} else if new_value > *value {
-					right.try_insert_with_candidate( new_value, value.clone() ).map(|new_right| Tree::Node {
-						left: left.clone(),
-						value: value.clone(),
-						right: Arc::new( new_right )
-					})
-				} else {
-					None
-				}
-			}
-		}
-	}
-	fn singleton(value: T) -> Self {
-		let empty: Arc<Tree<T>> = Arc::new( Tree::empty() );
-		Tree::Node { 
-			left: empty.clone(),
-			right: empty,
-			value: value
-		}
-	}
+    fn member_with_candidate(&self, search_value: T, best_candidate: T) -> bool {
+        return match *self {
+            Tree::Empty => search_value == best_candidate,
+            Tree::Node { ref left, ref value, ref right } => if search_value < *value {
+                left.member_with_candidate(search_value, best_candidate)
+            } else {
+                right.member_with_candidate(search_value, value.clone())
+            },
+        };
+    }
+    fn try_insert_with_candidate(&self, new_value: T, candidate: T) -> Option<Self> {
+        match *self {
+            Tree::Empty => {
+                if new_value == candidate {
+                    None
+                } else {
+                    Some(Tree::singleton(new_value))
+                }
+            }
+            Tree::Node { ref left, ref value, ref right } => {
+                if new_value < *value {
+                    left.try_insert_with_candidate(new_value, candidate).map(|new_left| {
+                        Tree::Node {
+                            left: Arc::new(new_left),
+                            value: value.clone(),
+                            right: right.clone(),
+                        }
+                    })
+                } else if new_value > *value {
+                    right.try_insert_with_candidate(new_value, value.clone()).map(|new_right| {
+                        Tree::Node {
+                            left: left.clone(),
+                            value: value.clone(),
+                            right: Arc::new(new_right),
+                        }
+                    })
+                } else {
+                    None
+                }
+            }
+        }
+    }
+    fn singleton(value: T) -> Self {
+        let empty: Arc<Tree<T>> = Arc::new(Tree::empty());
+        Tree::Node {
+            left: empty.clone(),
+            right: empty,
+            value: value,
+        }
+    }
 }
 
 
 
 #[test]
 fn empty_contains_nothing() {
-	let empty_tree: Tree<()> = Tree::empty();
+    let empty_tree: Tree<()> = Tree::empty();
 
-	// There is only one value of this type so this is exhaustive.
-	assert!( !empty_tree.member( () ) );
+    // There is only one value of this type so this is exhaustive.
+    assert!(!empty_tree.member(()));
 }
 
 #[test]
 fn inserted_values_are_contained() {
-	let tree = Tree::empty().insert( 3 ).insert( 5 );
+    let tree = Tree::empty().insert(3).insert(5);
 
-	assert!( tree.member(3) );
-	assert!( tree.member(5) );
-	assert!( !tree.member(42) );
+    assert!(tree.member(3));
+    assert!(tree.member(5));
+    assert!(!tree.member(42));
 }
--- src/stack.rs
+++ src/stack.rs
@@ -1,224 +1,233 @@
 use std::sync::Arc;
 
 #[derive(Debug)]
-enum StackError { NoSuchElementException, IndexOutOfRange }
+enum StackError {
+    NoSuchElementException,
+    IndexOutOfRange,
+}
 
 trait Stack<T: Clone> {
-	fn empty() -> Self;
-	fn is_empty(&self) -> bool;
-	fn cons(&self, value: T) -> Self;
-	fn head(&self) -> Result<T, StackError>;
-	fn tail(&self) -> Result<Arc<Self>, StackError>;
-	fn update(&self, i: u32, value: T) -> Result<Self,StackError>; 
-	fn size(&self) -> u32;
-	fn get(&self, i: u32) -> Result<T,StackError>;
+    fn empty() -> Self;
+    fn is_empty(&self) -> bool;
+    fn cons(&self, value: T) -> Self;
+    fn head(&self) -> Result<T, StackError>;
+    fn tail(&self) -> Result<Arc<Self>, StackError>;
+    fn update(&self, i: u32, value: T) -> Result<Self, StackError>;
+    fn size(&self) -> u32;
+    fn get(&self, i: u32) -> Result<T, StackError>;
 }
 
 #[derive(Debug, Clone)]
 enum CustomStack<T> {
-	Empty,
-	Cons { value: T, tail: Arc<CustomStack<T>> }
+    Empty,
+    Cons {
+        value: T,
+        tail: Arc<CustomStack<T>>,
+    },
 }
 
 impl<T: Clone> Stack<T> for CustomStack<T> {
-	fn empty() -> CustomStack<T> {
-		return CustomStack::Empty;
-	}
-	fn is_empty(&self) -> bool {
-		return match *self {
-			CustomStack::Empty => true,
-			_ => false,
-		}
-	}
-	fn cons(&self, value: T) -> Self {
-		return CustomStack::Cons { value: value, tail: Arc::new( self.clone() ) };
-	}
-	fn head(&self) -> Result<T, StackError> {
-		return match *self {
-			CustomStack::Empty => Err(StackError::NoSuchElementException),
-			CustomStack::Cons { ref value, .. } => Ok(value.clone())
-		}
-	}
-	fn tail(&self) -> Result<Arc<Self>, StackError> {
-		return match *self {
-			CustomStack::Empty => Err(StackError::NoSuchElementException),
-			CustomStack::Cons { ref tail, .. } => Ok(tail.clone())
-		}
-	}
-	fn update(&self, i: u32, new_value: T) -> Result<Self,StackError> {
-		return match *self {
-			CustomStack::Empty => Err(StackError::IndexOutOfRange),
-			CustomStack::Cons { ref value, ref tail } => match i {
-				0 => Ok(tail.clone().cons(new_value)),
-				_ => {
-					let updated_tail = try!(tail.update(i - 1, new_value));
-					Ok(updated_tail.cons(value.clone()))
-				}
-			}
-		}
-	}
-	fn size(&self) -> u32 {
-		return match *self {
-			CustomStack::Empty => 0,
-			CustomStack::Cons { ref tail, .. } => 1 + tail.size()
-		}
-	}
-	fn get(&self, i: u32) -> Result<T, StackError> {
-		return match *self {
-			CustomStack::Empty => Err(StackError::IndexOutOfRange),
-			CustomStack::Cons { ref value, ref tail } => match i {
-				0 => Ok(value.clone()),
-				_ => tail.get(i - 1)
-			}
-		}
-	}
+    fn empty() -> CustomStack<T> {
+        return CustomStack::Empty;
+    }
+    fn is_empty(&self) -> bool {
+        return match *self {
+            CustomStack::Empty => true,
+            _ => false,
+        };
+    }
+    fn cons(&self, value: T) -> Self {
+        return CustomStack::Cons {
+            value: value,
+            tail: Arc::new(self.clone()),
+        };
+    }
+    fn head(&self) -> Result<T, StackError> {
+        return match *self {
+            CustomStack::Empty => Err(StackError::NoSuchElementException),
+            CustomStack::Cons { ref value, .. } => Ok(value.clone()),
+        };
+    }
+    fn tail(&self) -> Result<Arc<Self>, StackError> {
+        return match *self {
+            CustomStack::Empty => Err(StackError::NoSuchElementException),
+            CustomStack::Cons { ref tail, .. } => Ok(tail.clone()),
+        };
+    }
+    fn update(&self, i: u32, new_value: T) -> Result<Self, StackError> {
+        return match *self {
+            CustomStack::Empty => Err(StackError::IndexOutOfRange),
+            CustomStack::Cons { ref value, ref tail } => match i {
+                0 => Ok(tail.clone().cons(new_value)),
+                _ => {
+                    let updated_tail = try!(tail.update(i - 1, new_value));
+                    Ok(updated_tail.cons(value.clone()))
+                }
+            },
+        };
+    }
+    fn size(&self) -> u32 {
+        return match *self {
+            CustomStack::Empty => 0,
+            CustomStack::Cons { ref tail, .. } => 1 + tail.size(),
+        };
+    }
+    fn get(&self, i: u32) -> Result<T, StackError> {
+        return match *self {
+            CustomStack::Empty => Err(StackError::IndexOutOfRange),
+            CustomStack::Cons { ref value, ref tail } => match i {
+                0 => Ok(value.clone()),
+                _ => tail.get(i - 1),
+            },
+        };
+    }
 }
 
 // Only compile this in tests to stop compiler whining.
 #[cfg(test)]
 fn suffixes<T: Clone>(stack: &Arc<CustomStack<T>>) -> CustomStack<Arc<CustomStack<T>>> {
-	let tail_suffixes = match **stack {
-		CustomStack::Empty => CustomStack::empty(),
-		CustomStack::Cons { ref tail, .. } => suffixes(&tail)
-	};
+    let tail_suffixes = match **stack {
+        CustomStack::Empty => CustomStack::empty(),
+        CustomStack::Cons { ref tail, .. } => suffixes(&tail),
+    };
 
-	return tail_suffixes.cons( stack.clone() );
+    return tail_suffixes.cons(stack.clone());
 }
 
 
 #[test]
 fn empty_is_empty() {
-	let stack: CustomStack<()> = CustomStack::empty();
+    let stack: CustomStack<()> = CustomStack::empty();
 
-	assert!( stack.is_empty() );
-	assert!( stack.size() == 0 );
+    assert!(stack.is_empty());
+    assert!(stack.size() == 0);
 }
 
 #[test]
 fn cons_is_not_empty() {
-	let stack: CustomStack<i32> = CustomStack::empty().cons(4);
+    let stack: CustomStack<i32> = CustomStack::empty().cons(4);
 
-	assert!( !stack.is_empty() );
-	assert!( stack.size() == 1 );
+    assert!(!stack.is_empty());
+    assert!(stack.size() == 1);
 }
 
 #[test]
 fn head_empty_error() {
-	let stack: CustomStack<()> = CustomStack::empty();
+    let stack: CustomStack<()> = CustomStack::empty();
 
-	assert!( stack.head().is_err() );
+    assert!(stack.head().is_err());
 }
 
 #[test]
 fn head_last_item() {
-	let stack: CustomStack<i32> = CustomStack::empty().cons(5).cons(6);
-	let head = stack.head();
+    let stack: CustomStack<i32> = CustomStack::empty().cons(5).cons(6);
+    let head = stack.head();
 
-	assert!( head.is_ok() );
-	assert!( head.unwrap() == 6 );
+    assert!(head.is_ok());
+    assert!(head.unwrap() == 6);
 }
 
 
 #[test]
 fn tail_empty_is_error() {
-	let stack: CustomStack<()> = CustomStack::empty();
+    let stack: CustomStack<()> = CustomStack::empty();
 
-	assert!( stack.tail().is_err() );
+    assert!(stack.tail().is_err());
 }
 
 
 #[test]
 fn head_after_tail() {
-	let stack: CustomStack<i32> = CustomStack::empty().cons(1).cons(2).cons(3);
-	let tailtail = stack.tail().unwrap().tail().unwrap();
+    let stack: CustomStack<i32> = CustomStack::empty().cons(1).cons(2).cons(3);
+    let tailtail = stack.tail().unwrap().tail().unwrap();
 
-	assert!( tailtail.head().unwrap() == 1 );
+    assert!(tailtail.head().unwrap() == 1);
 }
 
 #[test]
 fn size_multiple_items() {
-	let stack: CustomStack<i32> = CustomStack::empty().cons(1).cons(2).cons(3);
+    let stack: CustomStack<i32> = CustomStack::empty().cons(1).cons(2).cons(3);
 
-	assert!( stack.size() == 3 );
+    assert!(stack.size() == 3);
 }
 
 #[test]
 fn get_valid() {
-	let stack: CustomStack<i32> = CustomStack::empty().cons(1).cons(2).cons(3);
+    let stack: CustomStack<i32> = CustomStack::empty().cons(1).cons(2).cons(3);
 
-	assert!( stack.get(1).unwrap() == 2 );
+    assert!(stack.get(1).unwrap() == 2);
 }
 
 #[test]
 fn get_out_of_range() {
-	let stack: CustomStack<i32> = CustomStack::empty().cons(1).cons(2).cons(3);
+    let stack: CustomStack<i32> = CustomStack::empty().cons(1).cons(2).cons(3);
 
-	assert!( stack.get(3).is_err() );
+    assert!(stack.get(3).is_err());
 }
 
 #[test]
 fn cloneable() {
-	let stack: CustomStack<i32> = CustomStack::empty().cons(1).cons(2).cons(3);
-	let stack2 = stack.clone();
+    let stack: CustomStack<i32> = CustomStack::empty().cons(1).cons(2).cons(3);
+    let stack2 = stack.clone();
 
-	let tailtail = stack.tail().unwrap().tail().unwrap();
-	let tail = stack2.tail().unwrap();
+    let tailtail = stack.tail().unwrap().tail().unwrap();
+    let tail = stack2.tail().unwrap();
 
-	assert!( tailtail.head().unwrap() == 1 );
-	assert!( tail.head().unwrap() == 2 );
+    assert!(tailtail.head().unwrap() == 1);
+    assert!(tail.head().unwrap() == 2);
 }
 
 #[test]
 fn update_valid() {
-	let stack: CustomStack<i32> = CustomStack::empty().cons(1).cons(2).cons(3);
-	let updated = stack.clone().update(1,10).unwrap();
+    let stack: CustomStack<i32> = CustomStack::empty().cons(1).cons(2).cons(3);
+    let updated = stack.clone().update(1, 10).unwrap();
 
-	assert!(updated.size() == 3);
-	assert!(updated.get(0).unwrap() == 3);
-	assert!(updated.get(1).unwrap() == 10);
-	assert!(updated.get(2).unwrap() == 1);
+    assert!(updated.size() == 3);
+    assert!(updated.get(0).unwrap() == 3);
+    assert!(updated.get(1).unwrap() == 10);
+    assert!(updated.get(2).unwrap() == 1);
 
-	// And stack is unchanged (I think the typesystem ensures this?)
-	assert!(stack.size() == 3);
-	assert!(stack.get(0).unwrap() == 3);
-	assert!(stack.get(1).unwrap() == 2);
-	assert!(stack.get(2).unwrap() == 1);
+    // And stack is unchanged (I think the typesystem ensures this?)
+    assert!(stack.size() == 3);
+    assert!(stack.get(0).unwrap() == 3);
+    assert!(stack.get(1).unwrap() == 2);
+    assert!(stack.get(2).unwrap() == 1);
 }
 
 #[test]
 fn update_invalid() {
-	let stack: CustomStack<i32> = CustomStack::empty().cons(1).cons(2).cons(3);
-	let updated = stack.clone().update(4,10);
+    let stack: CustomStack<i32> = CustomStack::empty().cons(1).cons(2).cons(3);
+    let updated = stack.clone().update(4, 10);
 
-	assert!(updated.is_err());
+    assert!(updated.is_err());
 }
 
-#[test] 
+#[test]
 fn suffixes_empty() {
-	let stack: Arc<CustomStack<()>> = Arc::new( CustomStack::empty() );
-	let suffixes = suffixes(&stack);
+    let stack: Arc<CustomStack<()>> = Arc::new(CustomStack::empty());
+    let suffixes = suffixes(&stack);
 
-	assert!(suffixes.size() == 1);
-	assert!(suffixes.get(0).unwrap().is_empty() );
+    assert!(suffixes.size() == 1);
+    assert!(suffixes.get(0).unwrap().is_empty());
 }
 
-#[test] 
+#[test]
 fn suffixes_nonempty() {
-	let stack: Arc<CustomStack<i32>> = Arc::new( CustomStack::empty().cons(1).cons(2) );
-	let suffixes = suffixes(&stack);
+    let stack: Arc<CustomStack<i32>> = Arc::new(CustomStack::empty().cons(1).cons(2));
+    let suffixes = suffixes(&stack);
 
-	assert!( suffixes.size() == 3 );
+    assert!(suffixes.size() == 3);
 
-	let suffix1 = suffixes.get(0).unwrap();
-	assert!( suffix1.size() == 2 );
-	assert!( suffix1.get(0).unwrap() == 2 );
-	assert!( suffix1.get(1).unwrap() == 1 ); 
+    let suffix1 = suffixes.get(0).unwrap();
+    assert!(suffix1.size() == 2);
+    assert!(suffix1.get(0).unwrap() == 2);
+    assert!(suffix1.get(1).unwrap() == 1);
 
-	let suffix2 = suffixes.get(1).unwrap();
-	assert!( suffix2.size() == 1 );
-	assert!( suffix2.get(0).unwrap() == 1 );
+    let suffix2 = suffixes.get(1).unwrap();
+    assert!(suffix2.size() == 1);
+    assert!(suffix2.get(0).unwrap() == 1);
 
-	let suffix3 = suffixes.get(2).unwrap();
-	assert!( suffix3.is_empty() );
+    let suffix3 = suffixes.get(2).unwrap();
+    assert!(suffix3.is_empty());
 
 }
